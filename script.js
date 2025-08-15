ed81a2432810d38b77764b49ec63a35e32b92f4f
mosesolando82-cell <mosesolando82@gmail.com>
2025-08-15 07:56:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.querySelectorAll('.product').forEach(item => {
-    item.addEventListener('click', () => {
-        alert('Interested in this product? Contact us for special offers and more details 0240860087!');
-    });
-});
+    function greet() {
+     document.getElementById("message") .innerText= "moses":
+    } 
